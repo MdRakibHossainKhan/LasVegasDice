e6f2e518498b7006bba52a2395c671f3723fc9e0
Md. Rakib Hossain Khan <md.rakib.hossain.khan@gmail.com>
2021-01-06 19:19:12 +0600

    UI Design Completed

--- Las Vegas Dice/ViewController.swift
+++ Las Vegas Dice/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        rollButton.layer.cornerRadius = 8
     }
 
 
